27e941bb164a0c85ecb7482f1d62c49ea89438c8
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-05 17:43:43 +0200

    add task 11

---
+++ python-if_else_loops_functions/11-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+pow = __import__('11-pow').pow
+
+print(pow(2, 2))      # Affiche 4
+print(pow(98, 2))     # Affiche 9604
+print(pow(98, 0))     # Affiche 1
+print(pow(100, -2))   # Affiche 0.0001
+print(pow(-4, 5))     # Affiche -1024
