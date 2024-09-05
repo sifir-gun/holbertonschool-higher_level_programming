86ec4f353253e11720d230c7dbdd5de0e045207d
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-05 16:51:51 +0200

    delete :02d

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i != 8 or j != 9:
-            print("{:02d}{:02d}".format(i, j), end=", ")
+            print("{}{}".format(i, j), end=", ")
         else:
-            print("{:02d}{:02d}".format(i, j))
+            print("{}{}".format(i, j))
