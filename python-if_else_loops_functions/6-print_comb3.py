d1f7482d03b4486314b67b9a30307de43b0819f5
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-05 16:45:41 +0200

    add task 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i != 8 or j != 9:
+            print("{:02d}, {:02d}".format(i, j), end=", ")
+        else:
+            print("{:02d}{:02d}".format(i, j))
