e255a336882c7b889f3fcbfd20fc15309f4dad6f
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-06 10:53:23 +0200

    add task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-import hidden_4
+import sys
+import os
+
+# Add the directory containing 'hidden_4' to the Python module search path
+module_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(module_dir)
 
 if __name__ == "__main__":
+    import hidden_4
     names = dir(hidden_4)
     for name in sorted(names):
         if not name.startswith("__"):
