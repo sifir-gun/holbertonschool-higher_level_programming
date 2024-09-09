60064656fba5dae97cb2cd72c644a9b19a29747e
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-09 14:59:26 +0200

    task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,7 +3,6 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list  # Retourne la liste originale si l'indice est invalide
     
-    # Copie la liste pour ne pas modifier l'originale
-    new_list = my_list.copy()
-    new_list[idx] = element  # Remplace l'élément à la position donnée
-    return new_list  # Retourne la nouvelle liste
+    # Modifie l'élément de la liste à la position spécifiée
+    my_list[idx] = element
+    return my_list  # Retourne la liste modifiée
