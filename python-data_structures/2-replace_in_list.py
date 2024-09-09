db113fe0cc966d6e9cf45599a4160d185a34b44a
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-09 14:48:25 +0200

    cut the comment too long

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,5 +1,6 @@
 def replace_in_list(my_list, idx, element):
-    # Si l'index est trop petit ou trop grand, on ne change rien et on retourne la liste telle quelle
+    # Si l'index est trop petit ou trop grand, 
+    # on ne change rien et on retourne la liste telle quelle
     if idx < 0 or idx >= len(my_list):
         return my_list
     
