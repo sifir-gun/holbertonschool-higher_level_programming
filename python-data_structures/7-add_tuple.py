766088b7ecdf4f4f8d68723e46513c2752e9b149
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-09 15:53:10 +0200

    Add function to sum two tuples in 7-add_tuple.py

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Assigner une valeur par défaut de 0 pour les éléments manquants dans tuple_a
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    
+    # Assigner une valeur par défaut de 0 pour les éléments manquants dans tuple_b
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
+    
+    # Additionner les premiers éléments et les deuxièmes éléments
+    return (a1 + b1, a2 + b2)
