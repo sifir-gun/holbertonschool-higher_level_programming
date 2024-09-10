f8b63ce3458d446f9483c31eb2e49ddc89be2160
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-10 11:23:52 +0200

    Fix search and replace, and unique addition functions

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 def search_replace(my_list, search, replace):
-    # Create a new list where each element is replaced if it matches 'search'
-    return [replace if element == search else element for element in my_list]
+    # Return a new list where each element is replaced if it matches 'search'
+    return [replace if x == search else x for x in my_list]
--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,3 @@
 def uniq_add(my_list=[]):
-    # Use a set to eliminate duplicates and sum the unique values
+    # Use a set to ensure all values are unique, then sum the set
     return sum(set(my_list))
