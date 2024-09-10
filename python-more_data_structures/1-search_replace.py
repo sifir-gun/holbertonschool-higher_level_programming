f5f79ce0f308da694c123cc2d474d40ad343e83c
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-10 11:30:33 +0200

    Add missing shebang line to 1-search_replace.py and 2-uniq_add.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
     # Create a new list with replaced elements if they match 'search'
     new_list = [replace if x == search else x for x in my_list]
--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def uniq_add(my_list=[]):
     # Use a set to ensure all values are unique, then sum the set
     return sum(set(my_list))
