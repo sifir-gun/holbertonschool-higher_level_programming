470254776fb6d8dadd88c813b37b63c65917013f
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-11 11:07:00 +0200

    Fix handling of IndexError in safe integer printing function

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,14 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
+            # Tente d'imprimer l'élément en tant qu'entier
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
+            # Continue si l'élément ne peut pas être converti en entier
             continue
         except IndexError:
+            # Si l'index dépasse la taille de la liste, arrête la boucle
             break
-    print("")
+    print("")  # Imprime une nouvelle ligne à la fin
     return count
