3dcbab72b88bb47a97239fce744a99d56af80de7
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-13 18:35:55 +0200

    fix: handled missing arguments in say_my_name function

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -21,4 +21,7 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print(f"My name is {first_name} {last_name}")
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
