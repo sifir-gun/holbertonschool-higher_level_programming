86ef30cd7eeaea58a03c6543252bcffc22afc27a
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-13 10:11:57 +0200

    Handle special cases for float('inf') and float('nan') in add_integer function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -13,4 +13,11 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    # Vérification des valeurs spéciales inf et nan
+    if isinstance(a, float) and (a == float('inf') or a == float('nan')):
+        raise TypeError("a must be an integer")
+    if isinstance(b, float) and (b == float('inf') or b == float('nan')):
+        raise TypeError("b must be an integer")
+
     return int(a) + int(b)
