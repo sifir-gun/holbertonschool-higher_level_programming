38b36ea4c7e6e2108ee354fb4dee05b50bac6bcc
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-16 13:38:47 +0200

    all project and task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Ce module définit une classe vide Square qui représente un carré.
+"""
+
+class Square:
+    """
+    Une classe vide qui définit un carré.
+    """
+    passgit add.
