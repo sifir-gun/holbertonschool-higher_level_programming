bd00adfa4ec25bc06b9be3d572d7b6e73c9082b3
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-18 16:20:23 +0200

    add task 3

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""
+ Module for Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Initialise les instances de la classe Rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        Récupère la largeur du rectangle.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Définit la largeur du rectangle.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Récupère la hauteur du rectangle.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Définit la hauteur du rectangle.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        Retourne l'aire du rectangle.
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Retourne le périmètre du rectangle.
+        Si la largeur ou la hauteur est égale à 0, le périmètre est 0.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """
+        Retourne une représentation en chaîne de caractères du rectangle
+        avec le caractère '#'.
+        Si la largeur ou la hauteur est égale à 0, retourne une chaîne vide.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rect = []
+        for _ in range(self.__height):
+            rect.append("#" * self.__width)
+        return "\n".join(rect)
