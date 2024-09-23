9bf136d44444f728e6cdba8dcb2a69cde55cceff
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-23 16:35:51 +0200

    Fix function inherits_from to better check inheritance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -18,4 +18,4 @@ def inherits_from(obj, a_class):
     Returns:
         True if obj is an instance of a subclass of a_class, False otherwise.
     """
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
