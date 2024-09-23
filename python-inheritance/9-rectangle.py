7eda67dbbf0c033ad2c0d170621ec027b63bf26c
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-23 17:52:45 +0200

    Implement area and str methods in Rectangle class

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class that inherits from BaseGeometry.
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    A class that represents a rectangle, inheriting from BaseGeometry.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialize a new rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Returns the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle.
+
+        Returns:
+            str: String in the format [Rectangle] <width>/<height>.
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
