c863ced8c1eb5609da27883977db3c0385267cf3
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-24 10:23:24 +0200

    chmod

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-This module defines a class MyList that inherits from list
-and has a method to print the list in sorted order.
+Defines a class MyList that is a subclass of list with a method to print the list sorted.
 """
 
 
 class MyList(list):
     """
-    A custom list class that can print its elements in a sorted order.
+    A subclass of list that can print its elements in sorted order.
     """
+
     def print_sorted(self):
         """
         Prints the list in ascending sorted order.
