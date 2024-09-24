a1f001d249bd78cc24315dbf79da01ee4b39c38b
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-24 10:57:07 +0200

    Ajout de tests pour les nombres négatifs et la liste vide dans 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -11,15 +11,43 @@ Example usage:
 
     Create an instance of MyList and use its methods:
 
-        my_list = MyList()
-        my_list.append(1)
-        my_list.append(4)
-        my_list.append(2)
-        my_list.append(3)
-        my_list.append(5)
-        print(my_list)
-        my_list.print_sorted()
-        print(my_list)
+        >>> my_list = MyList()
+        >>> my_list.append(1)
+        >>> my_list.append(4)
+        >>> my_list.append(2)
+        >>> my_list.append(3)
+        >>> my_list.append(5)
+        >>> print(my_list)
+        [1, 4, 2, 3, 5]
+        >>> my_list.print_sorted()
+        [1, 2, 3, 4, 5]
+        >>> print(my_list)
+        [1, 4, 2, 3, 5]
+
+Additional tests with negative numbers:
+
+        >>> my_list = MyList()
+        >>> my_list.append(-1)
+        >>> my_list.append(-4)
+        >>> my_list.append(-2)
+        >>> my_list.append(-3)
+        >>> my_list.append(-5)
+        >>> print(my_list)
+        [-1, -4, -2, -3, -5]
+        >>> my_list.print_sorted()
+        [-5, -4, -3, -2, -1]
+        >>> print(my_list)
+        [-1, -4, -2, -3, -5]
+
+Test with an empty list:
+
+        >>> my_list = MyList()
+        >>> print(my_list)
+        []
+        >>> my_list.print_sorted()
+        []
+        >>> print(my_list)
+        []
 """
 
 
