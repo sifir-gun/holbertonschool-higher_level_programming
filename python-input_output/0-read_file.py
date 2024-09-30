bab74e5868675b74416138bceeccd330c93cbc24
sifir-gun <guney.tasdelen74@gmail.com>
2024-09-30 14:40:28 +0200

    docstring for task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """
+    Lit le contenu d'un fichier et l'affiche sur la sortie standard.
+
+    Args:
+        filename (str): Le chemin du fichier à lire.
+        Par défaut, une chaîne vide.
+    """
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end="")
