efe2c0d00ef5d7a43664b7864e0d7c2dde433b6e
sifir-gun <164861633+sifir-gun@users.noreply.github.com>
2024-10-01 10:32:32 +0200

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+"""
+Ce script contient une fonction qui lit le contenu d'un fichier et l'affiche
+sur la sortie standard.
+"""
+
 def read_file(filename=""):
     """
     Lit le contenu d'un fichier et l'affiche sur la sortie standard.
 
     Args:
-        filename (str): Le chemin du fichier à lire.
-        Par défaut, une chaîne vide.
+        filename (str, optional): Le chemin du fichier à lire.
+            Par défaut, une chaîne vide.
+
+    Returns:
+        None
     """
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end="")
