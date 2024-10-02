8d6fad998b75264ddea13f02409958d216f864c9
sifir-gun <guney.tasdelen74@gmail.com>
2024-10-02 09:24:29 +0200

    add task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+"""
+This script contains a function that writes text to a file.
+"""
+
+
 def write_file(filename="", text=""):
+    """
+    Writes text to a specified file.
+
+    Args:
+        filename (str, optional):
+        The path to the file where the text will be written.
+            Defaults to an empty string.
+        text (str, optional): The text to write to the file.
+            Defaults to an empty string.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
