9cfe03acee9e33d099593d803517e624deaaa05b
sifir-gun <guney.tasdelen74@gmail.com>
2024-10-02 09:27:36 +0200

    docstring

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+"""
+This script contains a function that appends text to a file.
+"""
+
+
 def append_write(filename="", text=""):
+    """
+    Appends text to a specified file.
+
+    Args:
+        filename (str, optional):
+        The path to the file where the text will be appended.
+            Defaults to an empty string.
+        text (str, optional): The text to append to the file.
+            Defaults to an empty string.
+
+    Returns:
+        int: The number of characters appended to the file.
+    """
     with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)
