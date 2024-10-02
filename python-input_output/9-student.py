080343fcba5dbf01ab0a979fc7a0e3caeef2193b
sifir-gun <guney.tasdelen74@gmail.com>
2024-10-02 10:08:07 +0200

    Add Student class with to_json method for dictionary representation

---
+++ python-input_output/9-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module defines the Student class.
+"""
+
+
+class Student:
+    """
+    Defines a student by their first name, last name, and age.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the student with first name, last name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of the Student instance.
+
+        Returns:
+            dict: A dictionary containing the student's attributes.
+        """
+        return self.__dict__
