1162e02feefd8271bdb79eaf066b412fe5a32d3a
sifir-gun <guney.tasdelen74@gmail.com>
2024-10-02 09:30:19 +0200

    docstring

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""
+This script contains a function that converts a JSON string to a Python object.
+"""
+
+
 import json
 
 
 def from_json_string(my_str):
+    """
+    Converts a JSON string to its corresponding Python object.
+
+    Args:
+        my_str (str): The JSON string to be converted.
+
+    Returns:
+        any: The Python object represented by the JSON string.
+    """
     return json.loads(my_str)
