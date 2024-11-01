0fedef6a57c61ccc6971a71e731747bb13aade6b
sifir-gun <guney.tasdelen74@gmail.com>
2024-11-01 15:26:40 +0100

    add 1 blan linek

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """
     Classe State qui correspond à la table 'states' dans la base de données.
