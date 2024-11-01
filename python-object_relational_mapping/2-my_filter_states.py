8507e625fb3a0ee19b030dea1a7e6f840a3dbad0
sifir-gun <guney.tasdelen74@gmail.com>
2024-11-01 14:03:06 +0100

    Add script for task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,41 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
+"""
+Lists all states where name matches the argument from the database hbtn_0e_0_usa
+
+Usage: ./2-my_filter_states.py <mysql_username> <mysql_password> <database_name> <state_name>
+"""
 
 if __name__ == "__main__":
-    # Connexion à la base de données
-    db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
+    import sys
+    import MySQLdb
 
-    # Création d'un curseur pour exécuter la requête
+    # Vérification du nombre d'arguments
+    if len(sys.argv) != 5:
+        print("Usage: ./2-my_filter_states.py <mysql_username> <mysql_password> <database_name> <state_name>")
+        sys.exit(1)
+
+    # Connexion à la base de données avec les arguments fournis
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    # Création d'un curseur pour exécuter des requêtes SQL
     cursor = db.cursor()
-    
-    # Exécution de la requête SQL en utilisant un argument de l'utilisateur pour le filtrage
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
-    
-    # Récupération et affichage des résultats
+
+    # Récupération du nom de l'état recherché
+    state_name_searched = sys.argv[4]
+
+    # Exécution de la requête SQL pour récupérer les états dont le nom correspond exactement à celui fourni
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(state_name_searched)
+    cursor.execute(query)
     rows = cursor.fetchall()
+
+    # Affichage des résultats
     for row in rows:
         print(row)
 
-    # Fermeture du curseur et de la connexion
+    # Fermeture du curseur et de la connexion à la base de données
     cursor.close()
     db.close()
